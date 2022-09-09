65bb12f66fc3da89a5641864032a1c5a6393edaf
Michelle Too <mjxtcodes@gmail.com>
2022-09-09 15:06:12 -0700

    commit tree 26ff1528dc2f86645fe18d0aa2260f51a943b434

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+let names = []
+let event = []
+
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+console.log(messages)
+
+function countDown(startingNumber) {
+    while (startingNumber > 0) {
+        console.log(startingNumber);
+        startingNumber--
+    }
+    console.log(startingNumber)
+}
+
+console.log(1 + 2 + "kittens")
+
